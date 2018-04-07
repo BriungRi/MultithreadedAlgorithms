71ef792a8815bb5ccf18fd35e3cfae0ed07f29a5
Brian <brnli7@gmail.com>
2018-04-07 15:55:44 -0400

    Basic merge sort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,16 +1,35 @@
+import sorting.MergeSortWorker;
 import utils.ListUtils;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class MergeSort {
-    public static int TEST_SIZE = 10000;
+    public static int TEST_SIZE = 9000000;
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter number of threads: ");
-        int numThreads = input.nextInt();
-
+    public static void main(String[] args) throws InterruptedException {
+        int numThreads = Integer.parseInt(args[0]);
         List<Integer> randomList = ListUtils.generateRandomList(TEST_SIZE);
+
+        long startTime = System.currentTimeMillis();
+
+        List<List<Integer>> partitions = ListUtils.partitionList(randomList, numThreads);
+        List<MergeSortWorker> mergeSortWorkers = new ArrayList<>();
+        for(int i = 0; i < partitions.size(); i++) {
+            MergeSortWorker mergeSortWorker = new MergeSortWorker(partitions.get(i));
+            mergeSortWorkers.add(mergeSortWorker);
+            mergeSortWorker.start();
+        }
+
+        for(MergeSortWorker worker : mergeSortWorkers)
+            worker.join();
+
+        List<Integer> sortedList = new ArrayList<>();
+        for(MergeSortWorker worker : mergeSortWorkers)
+            sortedList.addAll(worker.result());
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.printf("\nList of size %d sorted in %dms", TEST_SIZE, endTime - startTime);
     }
 }
--- src/sorting/MergeSortWorker.java
+++ src/sorting/MergeSortWorker.java
@@ -1,5 +1,21 @@
 package sorting;
 
-public class MergeSortWorker extends Thread{
+import java.util.Collections;
+import java.util.List;
 
+public class MergeSortWorker extends Thread{
+    private final List<Integer> list;
+
+    public MergeSortWorker(List<Integer> list) {
+        this.list = list; // this may need to be a copy bc shared memory
+    }
+
+    @Override
+    public void run() {
+        Collections.sort(list);
+    }
+
+    public List<Integer> result() {
+        return list;
+    }
 }
