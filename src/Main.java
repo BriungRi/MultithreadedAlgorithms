1183da93e19c86354b183b5625138488d414814b
Brian <brnli7@gmail.com>
2018-04-21 13:32:00 -0400

    Added a tester

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import sorting.MergeSort;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Running merge sort tests");
+        for(int listSize = 10_000; listSize <= 100_000; listSize += 10_000) {
+            runMergeSortTests(listSize);
+        }
+    }
+
+    private static void runMergeSortTests(int listSize) {
+        for(int i = 1; i <= 10; i++) {
+            MergeSort.mergeSort(i, listSize);
+        }
+        System.out.println();
+    }
+}
--- src/sorting/MergeSort.java
+++ src/sorting/MergeSort.java
@@ -1,3 +1,5 @@
+package sorting;
+
 import sorting.MergeSortWorker;
 import utils.ListUtils;
 
@@ -5,24 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MergeSort {
-    private static final int TEST_SIZE = 200000;
-
-    public static void main(String[] args) throws InterruptedException {
-        int numThreads = Integer.parseInt(args[0]);
-        List<Integer> randomList = ListUtils.generateRandomList(TEST_SIZE);
+    public static void mergeSort(int numThreads, int listSize) {
+        List<Integer> randomList = ListUtils.generateRandomList(listSize);
 
         long startTime = System.currentTimeMillis();
 
         List<List<Integer>> partitions = ListUtils.partitionList(randomList, numThreads);
         List<MergeSortWorker> mergeSortWorkers = new ArrayList<>();
-        for(int i = 0; i < partitions.size(); i++) {
-            MergeSortWorker mergeSortWorker = new MergeSortWorker(partitions.get(i));
+        for (List<Integer> partition : partitions) {
+            MergeSortWorker mergeSortWorker = new MergeSortWorker(partition);
             mergeSortWorkers.add(mergeSortWorker);
             mergeSortWorker.start();
         }
 
-        for(MergeSortWorker worker : mergeSortWorkers)
-            worker.join();
+        for(MergeSortWorker worker : mergeSortWorkers) {
+            try {
+                worker.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
 
         List<List<Integer>> sortedLists = new ArrayList<>();
         for(MergeSortWorker worker : mergeSortWorkers) {
@@ -32,7 +36,7 @@ public class MergeSort {
 
         if(ListUtils.isSorted(sortedList)) {
             long endTime = System.currentTimeMillis();
-            System.out.printf("\nList of size %d sorted in %dms", TEST_SIZE, endTime - startTime);
+            System.out.printf("\nList of size %d sorted in %dms with %d thread(s)", listSize, endTime - startTime, numThreads);
         }
     }
 }
